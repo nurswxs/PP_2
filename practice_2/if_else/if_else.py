96f643ca4cf3633915330b7b4c78fc4831f2f287
nurswxs <nursatdoskaliev@gmail.com>
2026-02-02 23:07:39 +0500

    Add files via upload

---
+++ practice_2/if_else/if_else.py
@@ -0,0 +1,22 @@
+#1
+age = 18
+if age >= 18:
+    print("adult")
+else:
+    print("minor")
+#2
+temperature = 22
+if temperature > 30:
+  print("It's hot outside!")
+else:
+  print("It's not hot outside!")
+#3
+number = 15
+if number > 0:
+  print("The number is positive")
+else:
+  print("The number is negative")
+#4
+x = 13
+if x >= 10 and x <= 20:
+    print("in range")
---
+++ practice_2/if_else/if_statement.py
@@ -0,0 +1,24 @@
+#1
+a = 456
+b = 567
+if b > a:
+  print("b is greater than a")
+#2
+age = 17
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+#3
+x = 1
+if x > 0:
+    print("positive")
+#4
+n = 3
+if n % 2 == 0:
+    print("even")
+#5
+a = 23
+b = 23
+if a == b:
+    print("equal")
